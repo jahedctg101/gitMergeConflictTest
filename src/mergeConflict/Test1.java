0c0599f9cad2112ce6860a59a5eaf3dea0b81d18
sdet_jahed <mohiuddin.ahmed.sdet@gmail.com>
2023-10-19 09:33:35 +0600

    1st commit

---
+++ src/mergeConflict/Test1.java
@@ -0,0 +1,12 @@
+package mergeConflict;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println("--- Line 1 ---");
+		System.out.println("--- Line 2 ---");
+		System.out.println("--- Line 3 ---");
+
+	}
+
+}
