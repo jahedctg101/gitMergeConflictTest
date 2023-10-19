452987e01c38d58f2f4d76cca7b43a988a962383
sdet_jahed <mohiuddin.ahmed.sdet@gmail.com>
2023-10-19 10:08:59 +0600

    commit 1

--- src/mergeConflict/Test1.java
+++ src/mergeConflict/Test1.java
@@ -4,9 +4,9 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("--- Line 1 ---");
-		System.out.println("--- Line 2 ---");
-		System.out.println("--- Line 3 ---");
+		
+		
+         	System.out.println("--- Line 3 ---");
                 System.out.println("--- NEW  Line 4 ---");
                 System.out.println("--- NEW  Line 5 ---");
 	}
