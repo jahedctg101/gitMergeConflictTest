e9b4713bb427e66bb7b73982136eff8c53e0a2d2
jahedctg101 <49084057+jahedctg101@users.noreply.github.com>
2023-10-19 09:52:49 +0600

    Update Test1.java

--- src/mergeConflict/Test1.java
+++ src/mergeConflict/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		
-
+		System.out.println("--- Line 1 ---");
 		System.out.println("--- Line 2 ---");
 		System.out.println("--- Line 3 ---");
                 System.out.println("--- NEW  Line 4 ---");
